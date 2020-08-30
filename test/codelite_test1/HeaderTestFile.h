7f845955f51d9a88ff9f004d5caef79005b1585f
Rhoona <rhooonabot@gmail.com>
2020-08-30 11:10:51 -0700

    test18

---
+++ test/codelite_test1/HeaderTestFile.h
@@ -0,0 +1,5 @@
+class HeaderTest
+{
+  public:
+    HeaderTest();
+};
---
+++ test/codelite_test1/main.cpp
@@ -0,0 +1,21 @@
+#include "HeaderTestFile.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv)
+{
+    cout << "Hello World" << std::endl;
+    cout << "testing this out" << endl;
+    cout << "wtf";
+    cout << "ok";
+
+    for(int count = 0; count < 5; count++)
+        cout << "test" << endl;
+
+    cout << "dinosau" << endl;
+    cout << "\n\n";
+
+    HeaderTest test1;
+
+    return 0;
+}
